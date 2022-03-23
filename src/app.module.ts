4d5f6591f27d56bfe83319db49c9a636623ef6d7
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-03-23 23:09:13 +0200

    created modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+
+// Modules
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+
+// Controllers
 import { AppController } from './app.controller';
+
+// Services
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
